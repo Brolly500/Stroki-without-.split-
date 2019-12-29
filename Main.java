72bc795c17c580b9589e3a6be08caa807b174722
Brolly500 <57621589+Brolly500@users.noreply.github.com>
2019-12-29 12:07:39 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void countGlas(){
+    static void GlasWord(){
         Scanner in = new Scanner(System.in);
         System.out.println("Введите строку");
         String str = " " + in.nextLine();
         int count = 0;
-        char[] pattern = new char[]{'а','о','э','и','у','ы','е','ё','ю','я','А','О','Э','И','У','Ы','Е','Ё','Ю','Я'};
+        char[] glas = new char[]{'а','о','э','и','у','ы','е','ё','ю','я','А','О','Э','И','У','Ы','Е','Ё','Ю','Я'};
         for (int i = 0; i < str.length() - 1; i++) {
             if(str.charAt(i) == ' '){
-                for (int j = 0; j < pattern.length; j++) {
-                    if(str.charAt(i + 1) == pattern[j]) count++;
+                for (int j = 0; j < glas.length; j++) {
+                    if(str.charAt(i + 1) == glas[j]) count++;
                 }
             }
         }
@@ -19,8 +19,7 @@ public class Main {
     }
 
 
-
-    static void length3(){
+    static void ThreeLetter(){
         Scanner in = new Scanner(System.in);
         System.out.println("Введите строку");
         String str = in.nextLine() + " %end";
@@ -36,24 +35,25 @@ public class Main {
         }
     }
 
-
-    static void Maxlength(){
+    static void MaxWord(){
         Scanner in = new Scanner(System.in);
         System.out.println("Введите строку");
         String str = in.nextLine() + " %end";
         Scanner strScanner = new Scanner(str);
         String word = strScanner.next(), maxWord = word;
+        int len = 0;
         while(word.compareTo("%end") != 0){
             word = strScanner.next();
             if(word.length() >= maxWord.length()) maxWord = word;
+            len = maxWord.length();
         }
         System.out.println("Слово максимальной длины:"+maxWord+"\n");
+        System.out.println("Длина слова " + len);
     }
 
-
     public static void main(String[] args) {
-        countGlas();
-        length3();
-        Maxlength();
+        GlasWord();
+        ThreeLetter();
+        MaxWord();
     }
 }
